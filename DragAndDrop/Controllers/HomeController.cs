e6450746835b752f817107079416d7d6a8873996
tt535619@gmail.com <tt535619@gmail.com>
2023-12-27 19:58:12 +0530

    Add project files.

---
+++ DragAndDrop/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using DragAndDrop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace DragAndDrop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IWebHostEnvironment _environment;
+
+
+        public HomeController(ILogger<HomeController> logger, IWebHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile files)
+        {
+            if (files != null && files.Length > 0)
+            {
+                var filename = "Files";
+                string filePath = Path.Combine(_environment.WebRootPath, filename);
+                if (!Directory.Exists(filePath))
+                {
+                    Directory.CreateDirectory(filePath);
+                }
+                var File = files.FileName;
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    files.CopyTo(stream);
+                }
+
+                return Json(new { success = true, message = "File uploaded successfully!" });
+            }
+
+            return Json(new { success = false, message = "Invalid file or empty file!" });
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DragAndDrop/wwwroot/js/site.js
@@ -0,0 +1,55 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    var dropArea = $('#dropArea');
+
+    dropArea.on('dragover', function (e) {
+        e.preventDefault();
+        dropArea.css('background-color', '#f5f5f5');
+    });
+
+    dropArea.on('dragleave', function () {
+        dropArea.css('background-color', '');
+    });
+
+    dropArea.on('drop', function (e) {
+        e.preventDefault();
+        dropArea.css('background-color', '');
+
+        var files = e.originalEvent.dataTransfer.files;
+        console.log(files);
+        handleFileUpload(files);
+    });
+
+    $('#dropArea').on("click", function (e) {
+        e.preventDefault();
+        var fileDialog = $('<input type="file">');
+        fileDialog.click();
+        fileDialog.change(function (e) {
+            var geekss = e.target.files;
+            console.log(geekss);
+            handleFileUpload(geekss);
+        });
+    });
+});
+
+function handleFileUpload(files) {
+    var formData = new FormData();
+    formData.append('files', files[0]);
+
+    $.ajax({
+        url: '/Home/UploadFile',
+        type: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (result) {
+            alert(result.message);
+        },
+        error: function () {
+            alert('Error uploading files.');
+        }
+    });
+}
